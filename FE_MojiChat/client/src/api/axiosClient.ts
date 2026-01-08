56b33a7569b4a51b9eaabcda842b3e208ab9e100
Tu TA <>
2026-01-08 11:15:00 +0700

    [TuTA] update code

--- FE_MojiChat/client/src/api/axiosClient.ts
+++ FE_MojiChat/client/src/api/axiosClient.ts
@@ -3,7 +3,7 @@
 import axios, { type InternalAxiosRequestConfig, type AxiosResponse, AxiosError } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL+"/api" || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
